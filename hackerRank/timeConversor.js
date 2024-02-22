d33f489da289f61ea2d1104569d4a2d2d463f440
Victor Monger <victormonger2@outlook.com>
2024-02-22 11:04:29 -0300

    time converter

---
+++ hackerRank/timeConversor.js
@@ -0,0 +1,13 @@
+const timeFormatConverter = (timeString) => {
+  const timeArray = timeString.split("");
+
+  const hours = timeArray.slice(0, 2).join("");
+  const minutes = timeArray.slice(3, 5).join("");
+  const seconds = timeArray.slice(6, 8).join("");
+  const dayPeriod = timeArray.slice(-2).join("").toUpperCase();
+
+  const militaryHours = dayPeriod === "PM" ? parseInt(hours) + 12 : hours;
+  const treatedMilitaryHours = militaryHours === 24 ? "00" : militaryHours;
+
+  return `${treatedMilitaryHours}:${minutes}:${seconds}`;
+};
