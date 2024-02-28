fc5d5714c44530dd733339c8957b9f67a58864a5
Victor Monger <victormonger2@outlook.com>
2024-02-27 21:57:59 -0300

    bubble sort

---
+++ algorithms/bubbleSort.js
@@ -0,0 +1,21 @@
+const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let swapped = false;
+
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        swapped = true;
+      }
+    }
+    if (swapped === false) break;
+  }
+  return arr;
+};
+
+const arr = [64, 34, 25, 12, 22, 11, 90];
+
+console.log(bubbleSort(arr));
