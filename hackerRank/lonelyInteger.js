cdb4ce62b3100d7b8a44cc9b8542171026f0926d
Victor Monger <victormonger2@outlook.com>
2024-03-01 20:33:45 -0300

    lonely integer less efficient

--- hackerRank/lonelyInteger.js
+++ hackerRank/lonelyInteger.js
@@ -1,5 +1,16 @@
 const uniqueOccurrence = (arr) => {
-  return arr.filter(element => arr.filter(value => element === value).length === 1)
+  let singleOccurrence = {};
+  for (let i = 0; i < arr.length; i++) {
+    let number = arr[i];  
+    singleOccurrence[number] = (singleOccurrence[number] || 0) + 1;
+    }
+
+  for (const number in singleOccurrence) {
+    if (singleOccurrence[number] === 1) {
+      return parseInt(number);
+    }
+  }
+  return null
 };
 
 const arr = [1, 2, 3, 4, 3, 2, 1];
