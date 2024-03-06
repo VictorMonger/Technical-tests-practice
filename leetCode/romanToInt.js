5183734a177b8832210377d22ff44568f0e8d992
Victor Monger <victormonger2@outlook.com>
2024-03-06 19:20:34 -0300

    roman to integer

---
+++ leetCode/romanToInt.js
@@ -0,0 +1,23 @@
+const romanToInt = (romanNumber) => {
+  const romanMap = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let result = 0;
+  for (let i = 0; i < romanNumber.length; i++) {
+    if (romanMap[romanNumber[i]] < romanMap[romanNumber[i + 1]]) {
+      result -= romanMap[romanNumber[i]];
+    } else {
+      result += romanMap[romanNumber[i]];
+    }
+  }
+  return result;
+};
+
+console.log(romanToInt("IV"));
